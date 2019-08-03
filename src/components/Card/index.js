83bfbec16490f76479a5dcb1f6502abc2487a2fc
unknown <r.dourado007@gmail.com>
2019-08-03 18:07:46 -0300

    Finalizando o projeto conforme apresentado no video da Rocketseat

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,16 @@
-import React, { useRef } from 'react'
+import React, { useRef, useContext } from 'react'
 import { useDrag, useDrop } from 'react-dnd'
+
+import BoardContext from '../Board/context'
 
 import { Container, Label } from './styles'
 
-export default function Card({ data, index }) {
+export default function Card({ data, index, listIndex }) {
   const ref = useRef();
+  const { move } = useContext(BoardContext)
 
   const [{ isDragging }, dragRef] = useDrag({
-    item: { type: 'CARD', index },
+    item: { type: 'CARD', index, listIndex },
     collect: monitor => ({
       isDragging: monitor.isDragging(),
     })
@@ -16,18 +19,21 @@ export default function Card({ data, index }) {
   const [, dropRef] = useDrop({
     accept: 'CARD',
     hover(item, monitor) {
-      const draggedIndex = index.index;
-      const targetIndex = index;
+      const draggedListIndex = item.listIndex
+      const targetListIndex = listIndex
 
-      if ( draggedIndex === targetIndex ) {
+      const draggedIndex = item.index
+      const targetIndex = index
+
+      if ( draggedIndex === targetIndex && draggedListIndex === targetListIndex ) {
         return;
       }
 
-      const targetSize = ref.current.getBoundingClientRect();
-      const targetCenter = (targetSize.bottom - targetSize.top) / 2;
+      const targetSize = ref.current.getBoundingClientRect()
+      const targetCenter = ((targetSize.bottom - targetSize.top) / 2)
 
-      const draggedOffset = monitor.getClientOffset();
-      const draggedTop = draggedOffset.y - targetSize.top;
+      const draggedOffset = monitor.getClientOffset()
+      const draggedTop = draggedOffset.y - targetSize.top
 
       if ( draggedIndex < targetIndex && draggedTop < targetCenter ) {
         return;
@@ -37,7 +43,10 @@ export default function Card({ data, index }) {
         return;
       }
 
-      
+      move(draggedListIndex, targetListIndex, draggedIndex, targetIndex)
+
+      item.index = targetIndex
+      item.listIndex = targetListIndex
     }
   })
 
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -5,7 +5,7 @@ import { Container } from './styles'
 
 import Card from '../Card'
 
-export default function List({ data }) {
+export default function List({ data, index: ListIndex }) {
   return (
     <Container done={data.done}>
       <header>
@@ -18,7 +18,14 @@ export default function List({ data }) {
       </header>
 
       <ul>
-        { data.cards.map( (card, index) => <Card key={card.id} index={index} data={card} />)}
+        { data.cards.map( (card, index) => (
+          <Card 
+            key={card.id} 
+            index={index} 
+            listIndex={ListIndex} 
+            data={card} />
+          ))
+        }
       </ul>
     </Container>
   );
